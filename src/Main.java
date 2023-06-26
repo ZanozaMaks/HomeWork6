3f3d929eded610cbf42a6f48f23837e95ba29382
Mak7im <mak7im2003@mail.ru>
2023-06-26 14:53:26 +0300

    1-10 задание

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,10 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
+        task9();
+        task10();
+
 
 
     }
@@ -67,7 +71,41 @@ public class Main {
             System.out.println(i);
         // Пишем код для задачи 7
     }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        int salary = 29000;
+        int jar = 0;
+        for (int i = 1; i < 13; i++) {
+            jar = jar + salary;
+            System.out.println("Месяц " + i + " Сумма " + jar);
+        }
+        // Пишем код для задачи 8
+    }
+
+    public static void task9() {
+        System.out.println("Задача 9");
+        int salary = 29000;
+        int bankAtInterest = 0;
+        for (int i = 1; i < 13; i++) {
+            bankAtInterest = bankAtInterest + bankAtInterest / 100;
+            bankAtInterest = bankAtInterest + salary;
+            System.out.println("Месяц " + i + " Сумма " + bankAtInterest);
+
+            // Пишем код для задачи 9
+        }
+    }
+
+    public static void task10() {
+        System.out.println("Задача 10");
+        for (int i = 2; i < 21; i = i + 1 * 2) {
+            System.out.println("Таблица умножения: " + i);
+        }
+        // Пишем код для задачи 10
+
+    }
 }
+
 
 
 
